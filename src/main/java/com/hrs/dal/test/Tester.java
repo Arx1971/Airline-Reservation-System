fe3952114a9407cc3dc44b50a426e9020948349c
Arx1971 <adnan.rahin13@gmail.com>
2019-11-15 05:27:13 -0500

    update

--- src/main/java/com/hrs/dal/test/Tester.java
+++ src/main/java/com/hrs/dal/test/Tester.java
@@ -8,9 +8,9 @@ import java.time.LocalDate;
 public class Tester {
 
     public static void main(String args[]) throws SQLException {
-        LocalDate ldObj = LocalDate.of(2017, 11, 6);
+        LocalDate localDate = LocalDate.of(2017, 11, 6);
         APIservice as = new APIservice();
-        as.getAllFlightsByAirline("American Airlines", ldObj);
+        as.getAllFlightsByAirline("American Airlines", localDate);
         as.getAllFlightsForReservation();
     }
 
