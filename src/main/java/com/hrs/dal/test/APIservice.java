4ef3fc39d169402f44ceedcf58206a1d09630c24
Arx1971 <adnan.rahin13@gmail.com>
2019-11-15 05:23:53 -0500

    update

--- src/main/java/com/hrs/dal/test/APIservice.java
+++ src/main/java/com/hrs/dal/test/APIservice.java
@@ -78,7 +78,46 @@ public class APIservice implements ServiceModule {
 
     @Override
     public Set<Flight> getAllFlightsForReservation() {
-        return Tester.testFlights();
+        Set<Flight> flights = new LinkedHashSet<>();
+
+        String query = "select flight_info.flight_info_id, airline_flight_info.airline_flight_id,airline_info.airline_id, \n" +
+                "source_name, destination_name, flight_status_info,flight_source_date, \n" +
+                "flight_dest_date,flight_max_capacity,flight_current_capacity,fare,airline_name, \n" +
+                "airline_flight_name, flight_fly_time, flight_land_time\n" +
+                "from flight_info, airline_info, airline_flight_info, flight_status\n" +
+                "where flight_info.airline_flight_id = airline_flight_info.airline_flight_id and\n" +
+                "airline_flight_info.airline_id = airline_info.airline_id and\n" +
+                "flight_status.airline_flight_id = airline_flight_info.airline_flight_id and\n" +
+                "flight_max_capacity > flight_current_capacity and\n" +
+                "flight_status_info = 'On Time'";
+
+        try {
+            Statement statement = this.connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+
+                Integer flightID = Integer.parseInt(rs.getString("flight_info.flight_info_id"));
+                String flightCode = Integer.toString(rs.getString("airline_flight_name").hashCode());
+                LocalDate sourceDate = LocalDate.parse(rs.getString("flight_source_date"));
+                Source source = new Source(rs.getString("source_name"), sourceDate, rs.getString("flight_fly_time"));
+                LocalDate destinationDate = LocalDate.parse(rs.getString("flight_dest_date"));
+                Destination destination = new Destination(rs.getString("destination_name"), destinationDate, rs.getString("flight_land_time"));
+                Integer capacity = Integer.parseInt(rs.getString("flight_max_capacity"));
+                Integer availableSeat = capacity - Integer.parseInt(rs.getString("flight_current_capacity"));
+                Float fare = Float.parseFloat(rs.getString("fare"));
+                String status = rs.getString("flight_status_info");
+                Airline airLine = new Airline(Integer.parseInt(rs.getString("airline_flight_info.airline_flight_id")), rs.getString("airline_name"));
+                Airplane airplane = new Airplane(Integer.parseInt(rs.getString("airline_info.airline_id")), rs.getString("airline_flight_name"));
+
+                Flight flight = new Flight(flightID, flightCode, source, destination, availableSeat, status, airLine, airplane, fare);
+                System.out.println(flight);
+                flights.add(flight);
+            }
+        } catch (SQLException e) {
+
+        }
+
+        return flights;
     }
 
     @Override
--- src/main/java/com/hrs/dal/test/Tester.java
+++ src/main/java/com/hrs/dal/test/Tester.java
@@ -11,7 +11,7 @@ public class Tester {
         LocalDate ldObj = LocalDate.of(2017, 11, 6);
         APIservice as = new APIservice();
         as.getAllFlightsByAirline("American Airlines", ldObj);
-
+        as.getAllFlightsForReservation();
     }
 
 }
